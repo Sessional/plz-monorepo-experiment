05e9d6741b39bf019f1dc3271af62a5ede455b18
Andrew Kruse <Sessional@users.noreply.github.com>
2022-10-08 03:35:34 -0500

    more complicated build dependency tree

--- services/service1/main.tf
+++ services/service1/main.tf
@@ -3,3 +3,12 @@ module "module1" {
   source            = "./platform-team/modules/module1"
   a_string_variable = "stuff"
 }
+
+module "module2" {
+  source = "./services/service1/modules/module1"
+}
+
+
+module "local_module1" {
+  source = "./modules/module2"
+}
--- terraform/prepare_module/main.py
+++ terraform/prepare_module/main.py
@@ -1,10 +1,15 @@
 import sys
 import argparse
 import shutil
+import logging
+import os
 
-print("preparing module with args: " + " ".join(sys.argv))
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
-parser = argparse.ArgumentParser(description='Process some integers.')
+log.info("preparing module with args: " + " ".join(sys.argv))
+
+parser = argparse.ArgumentParser(description='Prepare a terraform module')
 parser.add_argument('--pkg', type=str)
 parser.add_argument('--name', type=str)
 parser.add_argument('--module-dir', type=str)
@@ -12,6 +17,20 @@ parser.add_argument('--out', type=str)
 parser.add_argument('--out-dirs', type=str)
 parser.add_argument('--strip', type=str)
 parser.add_argument('--deps', type=str)
+parser.add_argument('--modules', type=str, nargs='+')
+
 args = parser.parse_args()
+print(args)
 
 shutil.move(args.module_dir, args.out)
+
+modules = args.modules
+
+for module in args.modules:
+    if module is '':
+        # if modules is left blank, the way it is passed results in a list with an empty string
+        # maybe just pretend like that doesn't happen and continue on with our day
+        continue
+    module_path = module.rstrip("/" + os.environ.get("TERRAFORM_TARGET"))
+    log.info(f"Moving {module} to {args.out}/{module_path}")
+    shutil.copytree(module, args.out + "/" + module_path)
