cf92d3c54c7f1a12bb62d9b5ba055937a5cf3485
Andrew Kruse <Sessional@users.noreply.github.com>
2022-10-08 05:33:24 -0500

    adding cmd variations to specify environments
    in addition supports passing variable files and backend
    config files by default

--- services/service1/main.tf
+++ services/service1/main.tf
@@ -1,13 +1,15 @@
+terraform {
+  backend "local" {}
+}
 
 module "module1" {
   source            = "./platform-team/modules/module1"
-  a_string_variable = "stuff"
+  a_string_variable = var.module_string
 }
 
 module "module2" {
   source = "./services/service1/modules/module1"
 }
-
 
 module "local_module1" {
   source = "./modules/module2"
---
+++ services/service1/variables.tf
@@ -0,0 +1,3 @@
+variable "module_string" {
+  type = string
+}
